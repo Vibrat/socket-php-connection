97015034b898774d94ff2882f69cbc5619a30616
DESKTOP-G47CM0M\admin <lam.nguyen@onsolve.com>
2018-12-19 11:28:24 +0700

    [SKPHP] Add multiple connections

--- functions.php
+++ functions.php
@@ -19,4 +19,32 @@ class CoreFunction {
 
         return $response;
     }
+
+    // Unmask incoming framed message
+    function unmask($text) {
+        $length = ord($text[1]) & 127;
+        if($length == 126) {
+            $masks = substr($text, 4, 4);
+            $data = substr($text, 8);
+        }
+        elseif($length == 127) {
+            $masks = substr($text, 10, 4);
+            $data = substr($text, 14);
+        }
+        else {
+            $masks = substr($text, 2, 4);
+            $data = substr($text, 6);
+        }
+        $text = "";
+        for ($i = 0; $i < strlen($data); ++$i) {
+            $text .= $data[$i] ^ $masks[$i%4];
+        }
+        return $text;
+    }
+
+    function arrayMapSocket($array, $name) {
+        return (count($array) ? array_map (function($item) use ($name) {
+            return $item[$name];
+        }, $array) : []);
+    }
 }
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+set_time_limit(0);
+
 require_once "./functions.php";
 $core = new CoreFunction();
 
@@ -7,33 +9,46 @@ $core = new CoreFunction();
 $host = "127.0.0.1";
 $port = "88";
 
-$clients = [];
-$buf = '';
-
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
 socket_bind($socket, $host, $port);
 socket_listen($socket);
+socket_set_nonblock($socket);
+
+$clients = [];
 
 while(true) {
-    if ($connection = socket_accept($socket)) {
+        
+    ## getting new connection
+    $connection = socket_accept($socket);
+    
+    ## check it's new
+    if (!in_array($connection, $core->arrayMapSocket($clients, 'socket')) && $connection) {
 
         ## Making handsake
         $response = socket_read($connection, 512);       
+        
         usleep(1000);
         socket_write($connection, 
         "HTTP/1.1 101 Switching Protocols\r\n" 
         . "Upgrade: websocket\r\n" 
         . "Connection: Upgrade\r\n" 
         . "Sec-WebSocket-Accept: ". base64_encode(sha1($core->parseHTTP($response)['Sec-WebSocket-Key'] . "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", true )) ."\r\n\r\n");
-
-        $clients[] = $connection;
-
-        ## Start Communication
-        $buf = socket_read($connection, 100000, PHP_BINARY_READ);
-        echo " data sent : " . $buf . "\n";
         
+        $clients[] = [ 'socket' => $connection, 'timestamp' => strtotime('now')];
+    }
+   
+    ## getting message
+    foreach($clients as $index=>$client) {
+
+        while ($bytes = socket_recv($client['socket'], $buf, 1024, 0) > 0) {          
+                ## Start Communication
+                echo " data sent : " . $client['socket'] . ": " . $core->unmask($buf) . "\n";
+        }
+
+        ## remove inconnected connection after 10 seconds
+        if (strtotime('now - 10 seconds') >= $client['timestamp']) {
+             unset($clients[$index]);
+        }
     }
 }
-
-socket_close($socket);
